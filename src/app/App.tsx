1258da71e29f509d51179eb382c655d20f249f17
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-11-22 19:10:00 +0300

    feat: add basic routing, main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { MainPage } from "../pages/MainPage";
+
 export function App() {
+    const router = createBrowserRouter(createRoutesFromElements(
+        <>
+            <Route index element={<MainPage />} />
+            <Route path="/home" element={<MainPage />} />
+        </>
+    ));
+
     return (
-        <div>App</div>
+        <RouterProvider router={router} />
     )
 }
